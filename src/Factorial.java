5ad03fe3c1ca9e8d8149d855c519fd871b5fd10a
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-11-29 13:41:33 +0100

    Variables de la clase Factorial renombradas

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,38 +1,30 @@
-
 package factorial;
-
 
 public class Factorial {
 
-
     public static void main(String[] args) {
 
+        int numero;
+        int factorial;
 
-    int j;
-    int f;
+        numero = 8;
 
-  
-   j = 8;    
-   
+        int i;
+/*
+        La variable numero es el numero del que se va a hacer el factorial, la variable factorial es
+        es el resultado del factorial de ese numero y la variable i es el contador
+*/
+        if (numero == 0) {
+            factorial = 1;
+        } else {
+            factorial = 1;
+            for (i = numero; i >= 1; i--) {
+                factorial = factorial * i;
+            }
+        }
 
-    
-   int i;
-   if (j == 0)
-f = 1;
-   else
-   {
-f = 1;
-for (i = j; i >= 1; i--)
-{
-    f = f * i;
-}
-   }    
-  
- 
-
-System.out.println(f); 
-
+        System.out.println(factorial);
 
     }
-    
+
 }
